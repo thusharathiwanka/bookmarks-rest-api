48cb6fa7437e0ba59490d9a22e5c41d700a4cf8e
Thushara Thiwanka <46595464+thusharathiwanka@users.noreply.github.com>
2022-04-22 20:44:28 +0530

    UPDATE: project refactored

---
+++ src/__init__.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask, jsonify
+
+def create_app(test_config = None):
+  app = Flask(__name__, instance_relative_config = True)
+
+  if test_config is None:
+    app.config.from_mapping(SECRET_KEY = os.environ.get('SECRET_KEY'))
+  else:
+    app.config.from_mapping(test_config)
+
+  @app.get('/')
+  def index():
+    return jsonify({'message': 'Hello World!'})
+  
+  return app
